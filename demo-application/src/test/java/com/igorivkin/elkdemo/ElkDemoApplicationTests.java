51eff72a3ed64f2f65ec4cbb5e6fc89e71198528
unknown <group06vp1@yandex.ru>
2021-10-23 10:14:38 +0300

    Первый коммит

---
+++ demo-application/src/test/java/com/igorivkin/elkdemo/ElkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorivkin.elkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
